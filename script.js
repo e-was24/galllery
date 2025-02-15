e7e553a93820b01673125e5e253f0a95e5365995
Haii <168735717+e-was24@users.noreply.github.com>
2025-02-15 21:55:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ confirm("clue, Your P : R****E****")
 
 function checkPassword() {
     var userPass = document.getElementById('UserPass').value;
-    var correctPassword = 'R1058E2568';
+    var correctPassword = '10000100010';
 
     if(userPass == correctPassword) {
         window.location.href = 'Beranda/home.html';
